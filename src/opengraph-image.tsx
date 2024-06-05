b8c73ad3df6fbbfcb0ebd1ee7d21115df7329acb
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-06-05 16:48:03 +0300

    added opengraph image

---
+++ src/opengraph-image.tsx
@@ -0,0 +1,47 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const alt = "Tally";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  const interSemiBold = fetch(
+    new URL("../../public/GrenzeGotisch-Medium.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 428,
+          background: "transparent",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "#005531",
+        }}
+      >
+        V
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "JetBrains Mono",
+          data: await interSemiBold,
+          style: "italic",
+          weight: 800,
+        },
+      ],
+    },
+  );
+}
