4c31edfc34e1725bc9e928c18cf68b44f5a42fb3
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-06-05 16:44:59 +0300

    added suspense

--- src/app/courses/add/page.tsx
+++ src/app/courses/add/page.tsx
@@ -1,165 +1,21 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  FormField,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormMessage,
-  Form,
-} from "@/components/ui/form";
-import { useForm } from "react-hook-form";
-import { Input } from "@/components/ui/input";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import Header from "@/components/Header";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense } from "react";
+import AddCourse from "./times/page";
+import Spinner from "@/components/Spinner";
 
-const formSchema = z.object({
-  name: z
-    .string()
-    .min(1, {
-      message: "Course name is required",
-    })
-    .max(128, {
-      message: "Course name is too long",
-    }),
-  days: z
-    .array(z.string(), {
-      required_error: "Select at least one day",
-    })
-    .min(1, {
-      message: "Select at least one day",
-    }),
-});
-
-export default function AddCourse() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      days: [],
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    router.push(`/courses/add/times?name=${values.name}&days=${values.days}`);
-  }
-
-  useEffect(() => {
-    const name = searchParams.get("name") || "";
-    const days = searchParams.get("days") || "";
-
-    const allDays = [
-      "sunday",
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday",
-    ];
-
-    if (name?.length >= 1 || name?.length <= 128) {
-      form.setValue("name", name);
-    }
-
-    if (days.split(",").filter((d) => allDays.includes(d)).length > 0) {
-      form.setValue(
-        "days",
-        days.split(",").filter((d) => allDays.includes(d)),
-      );
-    }
-  }, [pathname]);
-
+export default function Page() {
   return (
     <main>
       <Header />
-      <section className="flex h-[calc(100dvh-4.5rem)] flex-col items-center justify-start">
-        <div className="w-full max-w-lg">
-          <CardHeader className="p-4">
-            <CardTitle>Add a new course</CardTitle>
-          </CardHeader>
-          <CardContent className="p-4 pt-2">
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8 text-start"
-              >
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel htmlFor={field.name}>Course name</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Accounting"
-                          {...field}
-                          autoComplete="name"
-                          className="leading-normal"
-                          id={field.name}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="days"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel asChild>
-                        <p>Select days</p>
-                      </FormLabel>
-                      <ToggleGroup
-                        type="multiple"
-                        className="flex-wrap justify-start"
-                        onValueChange={(v) => field.onChange(v)}
-                        {...field}
-                      >
-                        <ToggleGroupItem variant="outline" value="sunday">
-                          Sun
-                        </ToggleGroupItem>
-                        <ToggleGroupItem variant="outline" value="monday">
-                          Mon
-                        </ToggleGroupItem>
-                        <ToggleGroupItem variant="outline" value="tuesday">
-                          Tue
-                        </ToggleGroupItem>
-                        <ToggleGroupItem variant="outline" value="wednesday">
-                          Wed
-                        </ToggleGroupItem>
-                        <ToggleGroupItem variant="outline" value="thursday">
-                          Thu
-                        </ToggleGroupItem>
-                        <ToggleGroupItem variant="outline" value="friday">
-                          Fri
-                        </ToggleGroupItem>
-                        <ToggleGroupItem variant="outline" value="saturday">
-                          Sat
-                        </ToggleGroupItem>
-                      </ToggleGroup>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="float-right">
-                  Next
-                </Button>
-              </form>
-            </Form>
-          </CardContent>
-        </div>
-      </section>
+      <Suspense
+        fallback={
+          <section className="flex h-[calc(100dvh-4.5rem)] flex-col items-center justify-center px-4 pb-32 text-center">
+            <Spinner size="lg" />
+          </section>
+        }
+      >
+        <AddCourse />
+      </Suspense>
     </main>
   );
 }
