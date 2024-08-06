451caa1edc9b5804585814786ac8e3aeaed8c069
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-08-07 02:05:54 +0300

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,31 +2,21 @@ import { auth } from "@/utils/auth";
 import { NextResponse } from "next/server";
 
 export default auth((req) => {
-  console.log(req.auth, req.nextUrl.pathname);
-
   if (req.nextUrl.pathname === "/home" || req.nextUrl.pathname === "/privacy") {
-    console.log("pathname is home or privacy");
-
     return NextResponse.next();
   }
 
   if (!req.auth && req.nextUrl.pathname !== "/login") {
-    console.log("redirecting to login");
-
     const newUrl = new URL("/login", req.nextUrl.origin);
 
     return Response.redirect(newUrl);
   }
 
   if (req.auth && req.nextUrl.pathname === "/login") {
-    console.log("redirecting to home");
-
     const newUrl = new URL("/", req.nextUrl.origin);
 
     return Response.redirect(newUrl);
   }
-
-  console.log("next");
 
   return NextResponse.next();
 });
