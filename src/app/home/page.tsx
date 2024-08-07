813c4e8691725726057625d4764d554684603e42
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-08-08 02:39:07 +0300

    updated home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,50 +1,50 @@
 import buttonStyles from "@/utils/styles/button";
 import Image from "next/image";
 import Link from "next/link";
+import { GeistSans } from "geist/font/sans";
 
 export default function Page() {
   return (
-    <>
-      <div className="relative flex h-dvh flex-grow flex-col overflow-x-hidden">
-        <div className="flex h-dvh flex-none flex-col">
-          <div className="flex flex-none items-center justify-center pt-8">
-            <Image src="/logo.svg" alt="Tally logo" height={32} width={23} />
-          </div>
-          <div className="flex flex-grow flex-col items-center justify-center gap-5 px-5 pb-[35vh] text-center md:pb-0 md:pr-[40%]">
-            <h1 className="max-w-[50rem] text-display-sm font-semibold md:text-display-lg">
-              Effortlessly Track and Manage Attendance with Tally
-            </h1>
-            <p className="text-secondary max-w-[50rem] text-text-lg md:text-text-xl">
-              Tally is a simple attendance tracker that helps you keep track of
-              attendance for your courses and classes.
-            </p>
-            <Link
-              href="/"
-              className={buttonStyles(
-                {
-                  size: "md",
-                  variant: "primary"
-                },
-                "mt-3 md:mt-5"
-              )}
-            >
-              Go to app
-            </Link>
-          </div>
-          <div className="absolute right-1/2 top-full h-[90vh] w-[69vh] -translate-y-[40%] translate-x-1/2 md:right-0 md:top-1/2 md:-translate-y-1/2 md:translate-x-[calc(60%-20vw)]">
-            <Image src="/logo.svg" alt="Tally logo" fill />
-          </div>
-        </div>
-        <footer className="text-tertiary p-2 pt-[70vh] text-center text-text-sm md:pt-0">
-          <p>
-            &copy; {new Date().getFullYear()} Tally. All rights reserved.{" "}
-            <Link className="underline underline-offset-2" href="/privacy">
-              Privacy policy
-            </Link>
-            .
+    <div className="flex flex-grow flex-col overflow-x-hidden">
+      <div className="grid flex-none gap-y-20 lg:h-dvh lg:grid-cols-2">
+        <div className="flex flex-col items-center justify-center gap-8 px-5 pt-32 text-center lg:pt-0">
+          <h1
+            className={`max-w-[50rem] text-display-sm font-semibold md:text-display-lg ${GeistSans.className}`}
+          >
+            Effortlessly Track and Manage Attendance with Tally
+          </h1>
+          <p
+            className={`text-secondary max-w-[50rem] text-text-lg md:text-text-xl ${GeistSans.className}`}
+          >
+            Tally is a simple attendance tracker that helps you keep track of
+            attendance for your courses and classes.
           </p>
-        </footer>
+          <Link
+            href="/"
+            className={buttonStyles(
+              {
+                size: "md",
+                variant: "primary"
+              },
+              "mt-3"
+            )}
+          >
+            Go to app
+          </Link>
+        </div>
+        <div className="relative mb-20 h-[70vh] w-full max-w-[70vw] self-center justify-self-center lg:mb-0">
+          <Image src="/logo.svg" alt="Tally logo" fill />
+        </div>
       </div>
-    </>
+      <footer className="text-tertiary p-2 text-center text-text-sm">
+        <p>
+          &copy; {new Date().getFullYear()} Tally. All rights reserved.{" "}
+          <Link className="underline underline-offset-2" href="/privacy">
+            Privacy policy
+          </Link>
+          .
+        </p>
+      </footer>
+    </div>
   );
 }
